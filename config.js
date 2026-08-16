00b2fbb8914c99ffc195f49a27dd9c649046819c
Elshorihy <sheenomatp@gmail.com>
2026-08-16 03:23:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
-// Firebase Web config for Elshori7y
-// Leave null to keep the site usable before Firebase is configured.
-window.ELSHORI7Y_FIREBASE_CONFIG = null;
-
-// Later replace null with:
-// window.ELSHORI7Y_FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_QGDcj0N-vka-QiqCTvR5IbReMp4W8Yo",
+  authDomain: "elshori7y-web.firebaseapp.com",
+  databaseURL: "https://elshori7y-web-default-rtdb.firebaseio.com",
+  projectId: "elshori7y-web",
+  storageBucket: "elshori7y-web.firebasestorage.app",
+  messagingSenderId: "575090467505",
+  appId: "1:575090467505:web:e35e20a2a00d2359947890",
+  measurementId: "G-1T82WPQJKK"
+};
